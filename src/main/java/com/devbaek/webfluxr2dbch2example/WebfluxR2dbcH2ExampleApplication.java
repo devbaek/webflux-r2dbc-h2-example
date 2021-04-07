7dd169504eddef5f982b78fd73394ec59a783a98
bbaing4564 <bbaing4564@gmail.com>
2021-04-07 23:47:45 +0900

    Initial commit

---
+++ src/main/java/com/devbaek/webfluxr2dbch2example/WebfluxR2dbcH2ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.devbaek.webfluxr2dbch2example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxR2dbcH2ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxR2dbcH2ExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devbaek/webfluxr2dbch2example/WebfluxR2dbcH2ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbaek.webfluxr2dbch2example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxR2dbcH2ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
